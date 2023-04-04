b77a9a4f1c3089bcbfef8142f6d1d62167945383
DannOut <danielxdn@gmail.com>
2023-04-04 15:40:14 -0300

    feat(req 2 done): req 2 finshed

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,20 @@
+from collections import Counter
+from datetime import date
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(list):
+        manufaturing_date = [item["data_de_fabricacao"] for item in list]
+        expiration_date = [
+            item["data_de_validade"]
+            for item in list
+            if item["data_de_validade"] > str(date.today())
+        ]
+        companies = [item["nome_da_empresa"] for item in list]
+        company_with_more_products = Counter(companies).most_common()[0]
+        return (
+            f"Data de fabricação mais antiga: {min(manufaturing_date)}\n"
+            f"Data de validade mais próxima: {min(expiration_date)}\n"
+            f"Empresa com mais produtos: {company_with_more_products[0]}"
+        )
