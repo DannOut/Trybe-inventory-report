5a9b4a2d74dde85d50f2f745610675584db3ccc4
DannOut <danielxdn@gmail.com>
2023-04-04 17:28:29 -0300

    feat(testing refact): testing refact

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -5,17 +5,17 @@ from inventory_report.inventory.inventory_iterator import InventoryIterator
 
 class InventoryRefactor:
     def __init__(self, importer):
-        self.__simple_report = SimpleReport
-        self.__complete_report = CompleteReport
+        self.simple_report = SimpleReport
+        self.complete_report = CompleteReport
         self.importer = importer
-        self._data = []
+        self.data = []
 
     def import_data(self, file, type):
-        self._data.extend(self.importer.import_data(file))
+        self.data.extend(self.importer.import_data(file))
         if type == "simples":
-            return self.__simple_report.generate(self._data)
+            return self.simple_report.generate(self.data)
         elif type == "completo":
-            return self.__complete_report.generate(self._data)
+            return self.complete_report.generate(self.data)
 
     def __iter__(self):
-        return InventoryIterator(self._data)
+        return InventoryIterator(self.data)
