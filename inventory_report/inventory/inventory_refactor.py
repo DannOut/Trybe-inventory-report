12a0eec58d7330c76e57be5bcce6dea8dc3626e3
DannOut <danielxdn@gmail.com>
2023-04-04 17:18:43 -0300

    feat(finishing project): finishing project

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -5,17 +5,17 @@ from inventory_report.inventory.inventory_iterator import InventoryIterator
 
 class InventoryRefactor:
     def __init__(self, importer):
-        self._simple_report = SimpleReport
-        self._complete_report = CompleteReport
+        self.__simple_report = SimpleReport
+        self.__complete_report = CompleteReport
         self.importer = importer
         self._data = []
 
     def import_data(self, file, type):
         self._data.extend(self.importer.import_data(file))
         if type == "simples":
-            return self._simple_report.generate(self._data)
+            return self.__simple_report.generate(self._data)
         elif type == "completo":
-            return self._complete_report.generate(self._data)
+            return self.__complete_report.generate(self._data)
 
     def __iter__(self):
         return InventoryIterator(self._data)
--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,32 @@
+import sys
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+
+
 def main():
-    pass
+    if len(sys.argv) < 3:
+        return sys.stderr.write("Verifique os argumentos\n")
+
+    path = sys.argv[1]
+    report_type = sys.argv[2]
+
+    if path.endswith(".csv"):
+        importer = CsvImporter()
+    elif path.endswith(".json"):
+        importer = JsonImporter()
+    elif path.endswith(".xml"):
+        importer = XmlImporter()
+    else:
+        sys.stderr.write("Arquivo não suportado!\n")
+        return
+
+    dataset = InventoryRefactor(importer)
+    report = dataset.import_data(path, report_type)
+
+    print(report, end="")
+
+
+if __name__ == "__main__":
+    main()
