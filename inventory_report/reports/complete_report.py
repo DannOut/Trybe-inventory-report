fd24aff2dd7f41a5c1f4cf962a02b5ef7fca89cf
DannOut <danielxdn@gmail.com>
2023-04-04 15:48:34 -0300

    feat(finishing req3): finishing req3

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,20 @@
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
 class CompleteReport:
-    pass
+    @staticmethod
+    def generate(list):
+        simple_report = SimpleReport.generate(list)
+        companies = [product["nome_da_empresa"] for product in list]
+        company_products = Counter(companies).items()
+        companies_total = ""
+
+        for company, amount in company_products:
+            companies_total += f"- {company}: {amount}\n"
+
+        return (
+            f"{simple_report}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{companies_total}"
+        )
